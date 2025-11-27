e9cf07991561246bd2fe16dc9da2ad1c7d24ca83
Adam Piszczek <adampiszczek1904@gmail.com>
2025-11-27 15:45:23 +0100

    test

--- utils.py
+++ utils.py
@@ -4,3 +4,6 @@ import random
 def predict_price(area_m2, rooms, floor, year_built, longitude, latitude, locality):
     base_price = 5000 * area_m2
     return base_price + random.randint(-10000, 10000)
+
+
+
